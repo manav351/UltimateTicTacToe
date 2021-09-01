c3bbffdcd85d0304c7f212bce657fca1210c5e4e
Manav <manav12121999@gmail.com>
2021-09-02 04:18:35 +0530

    Adds 3x3x3 cells and Result check on all Individuals. packing & guiding left

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
-let grid1 = [0,0,0,0,0,0,0,0,0]
+// let grid = [[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]];
+let grid = [];
+for(let i=0;i<10;i++){
+    grid[i] = [0,0,0,0,0,0,0,0,0];
+}
 let counter = 1;
-function buttonpressed(val){
-    grid1[val-1] = counter;
+function buttonpressed(gridNumber,val){
+    grid[gridNumber][val-1] = counter;
     counter = counter == 1? 2:1;
-    if(grid1[val-1] == 1)document.getElementById(`box1cell${val}`).innerHTML = "\u274C";
-    else document.getElementById(`box1cell${val}`).innerHTML = "\u25EF";
-    document.getElementById(`box1cell${val}`).disabled = true;
-    checkResult();
+    if(grid[gridNumber][val-1] == 1)document.getElementById(`box${gridNumber}cell${val}`).innerHTML = "\u274C";
+    else document.getElementById(`box${gridNumber}cell${val}`).innerHTML = "\u25EF";
+    document.getElementById(`box${gridNumber}cell${val}`).disabled = true;
+    checkResult(gridNumber);
 }
-function checkResult(){
+function checkResult(gridNumber){
     for(let i=0;i<3;i++){
-        if(grid1[0 + (3*i)] == grid1[1+ (3*i)] && grid1[1+ (3*i)] == grid1[2+ (3*i)] && grid1[0 + (3*i)] !=0){
-            disableAllButtons();
+        if(grid[gridNumber][0 + (3*i)] == grid[gridNumber][1+ (3*i)] && grid[gridNumber][1+ (3*i)] == grid[gridNumber][2+ (3*i)] && grid[gridNumber][0 + (3*i)] !=0){
+            disableAllButtons(gridNumber);
+            grid[0][gridNumber] = [gridNumber][0 + (3*i)];
         }
-        if(grid1[0+i] == grid1[3 +i] && grid1[3+i] == grid1[6+i] && grid1[0+i] !=0){
-            disableAllButtons();
+        if(grid[gridNumber][0+i] == grid[gridNumber][3 +i] && grid[gridNumber][3+i] == grid[gridNumber][6+i] && grid[gridNumber][0+i] !=0){
+            disableAllButtons(gridNumber);
+            grid[0][gridNumber] = grid[gridNumber][0+i];
         }
     }
-    if(grid1[0] == grid1[4] && grid1[4] == grid1[8] && grid1[0] !=0){
-        disableAllButtons();
+    if(grid[gridNumber][0] == grid[gridNumber][4] && grid[gridNumber][4] == grid[gridNumber][8] && grid[gridNumber][0] !=0){
+        disableAllButtons(gridNumber);
+        grid[0][gridNumber] = grid[gridNumber][4];
     }
-    if(grid1[2] == grid1[4] && grid1[4] == grid1[6] && grid1[2] !=0){
-        disableAllButtons();
+    if(grid[gridNumber][2] == grid[gridNumber][4] && grid[gridNumber][4] == grid[gridNumber][6] && grid[gridNumber][2] !=0){
+        disableAllButtons(gridNumber);
+        grid[0][gridNumber] = grid[gridNumber][4];
     }
 }
-function disableAllButtons(){
+function disableAllButtons(gridNumber){
     for(let i=1;i<10;i++){
-        document.getElementById(`box1cell${i}`).disabled = true;
+        document.getElementById(`box${gridNumber}cell${i}`).disabled = true;
     }
 }
 // setInterval(checkResult, 1000);
