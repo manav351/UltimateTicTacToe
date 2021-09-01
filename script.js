972c971ba0cca3d5ab60cbb01d84a48b186b06fd
Manav <manav12121999@gmail.com>
2021-09-02 03:48:37 +0530

    Implements and Completes the 3x3 Tic Tac Toe

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+let grid1 = [0,0,0,0,0,0,0,0,0]
+let counter = 1;
 function buttonpressed(val){
-    console.log(val);
+    grid1[val-1] = counter;
+    counter = counter == 1? 2:1;
+    if(grid1[val-1] == 1)document.getElementById(`box1cell${val}`).innerHTML = "\u274C";
+    else document.getElementById(`box1cell${val}`).innerHTML = "\u25EF";
+    document.getElementById(`box1cell${val}`).disabled = true;
+    checkResult();
 }
+function checkResult(){
+    for(let i=0;i<3;i++){
+        if(grid1[0 + (3*i)] == grid1[1+ (3*i)] && grid1[1+ (3*i)] == grid1[2+ (3*i)] && grid1[0 + (3*i)] !=0){
+            disableAllButtons();
+        }
+        if(grid1[0+i] == grid1[3 +i] && grid1[3+i] == grid1[6+i] && grid1[0+i] !=0){
+            disableAllButtons();
+        }
+    }
+    if(grid1[0] == grid1[4] && grid1[4] == grid1[8] && grid1[0] !=0){
+        disableAllButtons();
+    }
+    if(grid1[2] == grid1[4] && grid1[4] == grid1[6] && grid1[2] !=0){
+        disableAllButtons();
+    }
+}
+function disableAllButtons(){
+    for(let i=1;i<10;i++){
+        document.getElementById(`box1cell${i}`).disabled = true;
+    }
+}
+// setInterval(checkResult, 1000);
